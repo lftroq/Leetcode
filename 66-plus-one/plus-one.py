6ecc296720d908a7f2bf6d5e601b602fee557a5f
lftroq <109480517+lftroq@users.noreply.github.com>
2025-06-01 15:52:37 +0700

    Time: 0 ms (100.00%) | Memory: 17.6 MB (79.46%) - LeetSync

---
+++ 66-plus-one/plus-one.py
@@ -0,0 +1,10 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        digits[-1] += 1
+        for i in range(len(digits)-1, -1, -1):
+            if(digits[i] == 10):
+                digits[i] = 0
+                if(i > 0): digits[i - 1] += 1
+                else: digits = [1] + digits
+            else: break
+        return digits
