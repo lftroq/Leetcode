b92a98dcf7203125dd472003aab21a9d688196ca
lftroq <109480517+lftroq@users.noreply.github.com>
2025-06-02 12:09:53 +0700

    Time: 72 ms (55.31%) | Memory: 242.8 MB (45.69%) - LeetSync

---
+++ 1294-range-frequency-queries/range-frequency-queries.cpp
@@ -0,0 +1,17 @@
+class RangeFreqQuery {
+public:
+    map<int,vector<int>> mp;
+    RangeFreqQuery(vector<int>& arr) {
+        for(int i=0;i<(int)arr.size();i++) mp[arr[i]].push_back(i);
+    }
+    
+    int query(int left, int right, int value) {
+        return upper_bound(mp[value].begin(),mp[value].end(),right)-lower_bound(mp[value].begin(),mp[value].end(),left);
+    }
+};
+
+/**
+ * Your RangeFreqQuery object will be instantiated and called as such:
+ * RangeFreqQuery* obj = new RangeFreqQuery(arr);
+ * int param_1 = obj->query(left,right,value);
+ */
