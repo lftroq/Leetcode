b4c468962bd19e9e7dd2f2602910cf90f99973f8
lftroq <109480517+lftroq@users.noreply.github.com>
2025-06-02 12:00:57 +0700

    Time: 0 ms (100.00%) | Memory: 19.8 MB (61.81%) - LeetSync

---
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        if(letters.back()<=target) return letters[0];
+        return *upper_bound(letters.begin(),letters.end(),target);
+    }
+};
