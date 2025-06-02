7f8746015d7659139745fa0ef3953437d76ae6e8
lftroq <109480517+lftroq@users.noreply.github.com>
2025-06-02 11:57:51 +0700

    Time: 173 ms (13.25%) | Memory: 180.4 MB (79.29%) - LeetSync

---
+++ 307-range-sum-query-mutable/range-sum-query-mutable.cpp
@@ -0,0 +1,44 @@
+class NumArray {
+public:
+    int n;
+    vector<int> bit, a;
+    NumArray(vector<int>& nums) {
+        n = (int)nums.size();
+        bit.resize(n + 1, 0);
+        a.resize(n + 1, 0);
+        for (int i = 0; i < n; i++) {
+            up(i + 1, nums[i]);
+            a[i + 1] = nums[i];
+        }
+    }
+
+    void up(int idx, int val) {
+        for (; idx <= n; idx += (idx & (-idx)))
+            bit[idx] += val;
+    }
+
+    void update(int index, int val) {
+        index++;
+        up(index, -a[index]);
+        a[index] = val;
+        up(index, a[index]);
+    }
+
+    int sumRange(int left, int right) {
+        cout << endl;
+        int ans = 0;
+        right++;
+        for (; right; right -= (right & (-right)))
+            ans += bit[right];
+        for (; left; left -= (left & (-left)))
+            ans -= bit[left];
+        return ans;
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * obj->update(index,val);
+ * int param_2 = obj->sumRange(left,right);
+ */
