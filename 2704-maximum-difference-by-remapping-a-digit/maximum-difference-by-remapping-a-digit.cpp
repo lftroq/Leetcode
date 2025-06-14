89e7247f650cf05f41c7614cdb34ff5e973a628a
lftroq <109480517+lftroq@users.noreply.github.com>
2025-06-14 15:09:28 +0700

    Time: 0 ms (100.00%) | Memory: 8 MB (84.91%) - LeetSync

--- 2704-maximum-difference-by-remapping-a-digit/maximum-difference-by-remapping-a-digit.cpp
+++ 2704-maximum-difference-by-remapping-a-digit/maximum-difference-by-remapping-a-digit.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int minMaxDifference(int num) {
-        string n=to_string(num);
-        string a=n,b=n;
+        string a=to_string(num);
+        string b=a;
         int j=0;
-        while(n[j]=='9') j++;
-        for(int i=(int)n.size()-1;i>=0;i--)
+        while(b[j]=='9') j++;
+        for(int i=(int)b.size()-1;i>=0;i--)
         {
             if(a[i]==a[0]) a[i]='0';
             if(b[i]==b[j]) b[i]='9';
